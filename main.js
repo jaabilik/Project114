35f1f812e4002ea8a2dffff9961ed6cd906a2c94
jaabilik <121368312+jaabilik@users.noreply.github.com>
2023-06-22 16:35:06 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload() {
+
+}
+
+function setup() {
+canvas = createCanvas(300, 300);
+canvas.position(485,250)
+video = createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Posenet will find you :3')
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y); 
+    }
+}
+
+function draw() {
+image(video, 0, 0, 300, 300);
+}
+
+function takeSnapshot() {
+    save('NOTClownimagequeationmark.png')
+}
